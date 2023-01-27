6eddf835fed33d9d5deaa77ec6d0bb57215be6fe
jonaszeferino <75401113+jonaszeferino@users.noreply.github.com>
2023-01-27 15:29:52 -0300

    reservations & Order waiting

--- pages/index.js
+++ pages/index.js
@@ -1,119 +1,23 @@
-/* eslint-disable react-hooks/rules-of-hooks */
-
-import { useState } from "react";
-import React from "react";
-import styles from "../styles/Home.module.css";
-import { format, differenceInDays } from "date-fns";
 import Link from "next/link";
-
-export default function orders() {
-  let [orderStock, setOrderStock] = useState([]);
-  let [orderUser, setOrderUser] = useState();
-  let [error, setError] = useState(null);
-  let [dayNumber, setDaysNumber] = useState(null);
-
-  const apiCall = (event) => {
-    const url = `https://production-order.omniplat.io/v1/clients/${orderUser}/fulfillments/locations/190410/status/WAITING?page=1`;
-
-    fetch(url, {
-      headers: new Headers({
-        Authorization: process.env.NEXT_PUBLIC_LE_AUTH,
-        "Content-Type": "application/json",
-      }),
-    })
-      .then((response) => {
-        if (response.status === 200) {
-          setError(null);
-          return response.json();
-        } else {
-          throw new Error("Dados Incorretos");
-        }
-      })
-      .then((result) => setOrderStock(result))
-      .catch((error) => setError(true));
-  };
-
+import Head from "next/head";
+export default function Home() {
   return (
     <div>
-      <h3 className={styles.title}>Pedidos Waiting</h3>
-      <h2 className={styles.grid}>
-        <br />
-        <label type="text">
-          Client:
-          <input
-            className={styles.card}
-            required={true}
-            type="text"
-            value={orderUser}
-            onChange={(event) => setOrderUser(event.target.value)}
-          ></input>
-        </label>
-
-        <button className={styles.card} onClick={apiCall}>
-          Verificar
-        </button>
-      </h2>
-
-      <ul>
-        {orderStock &&
-          orderStock.data &&
-          orderStock.data.length > 0 &&
-          orderStock.data.map((orders) => {
-            const isOutdated =
-              differenceInDays(new Date(), new Date(orders.createdAt)) > 5;
-            return (
-              <li key={orders.orderId}>
-                <span>
-                  Pedido: <strong> {orders.orderId}</strong>
-                </span>
-                <br />
-                <span>
-                  Dias Nesse Status:{" "}
-                  <strong>
-                    {" "}
-                    {differenceInDays(new Date(), new Date(orders.createdAt))}
-                  </strong>
-                </span>
-                {"  "}-
-                {isOutdated && (
-                  <span style={{ color: "red", font: "bold" }}>
-                    Pedido parado a mais de 5 dias
-                  </span>
-                )}
-                <br />
-                <span>
-                  Data do Pedido:
-                  <strong>
-                    {orders.createdAt.length > 0
-                      ? format(new Date(orders.createdAt), " dd/MM/yyyy")
-                      : ""}
-                  </strong>
-                  <br />
-                </span>
-                LocationId:{" "}
-                <span>
-                  <strong> {orders.locationId}</strong>
-                </span>
-                <br />
-                Canal:{" "}
-                <span>
-                  <strong> {orders.channelId}</strong>
-                </span>
-                <br />
-                <Link
-                  href={`https://oms.chaordic.com.br/deliveries/${orders.orderId}?channel=${orders.channelId}&fid=F1`}
-                  as="a"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <strong>Link Do Pedido</strong>
-                </Link>
-                <br />
-                <br />
-              </li>
-            );
-          })}
-      </ul>
+      <Head>
+        <title>Home</title>
+        <meta name="keywords" content="nba,franchise,teams"></meta>
+        <meta name="description" content="encontre tudo de nba aqui"></meta>
+      </Head>
+      <h1>Omni Tools</h1>
+      <br />
+      <Link href="/orderwaiting">
+        <a>Pedidos Em Espera</a>
+      </Link>
+      <br />
+      <Link href="/reservations">
+        <a>Reservas Em Aberto</a>
+      </Link>
+      <br />
     </div>
   );
 }
---
+++ pages/reservations.js
@@ -0,0 +1,88 @@
+import { useState } from "react";
+import styles from "../styles/Home.module.css";
+import ErrorPage from "./error-page";
+import { format } from "date-fns";
+
+export default function Reservations() {
+  let [reservationStock, setReservationStock] = useState([]);
+  let [reservationUser, setReservationUser] = useState();
+  let [reservationBasic, setReservationBasic] = useState();
+  let [isError, setError] = useState(false);
+
+  const apiCall = (event) => {
+    const url = `https://hub.omniplat.io/v1/clients/${reservationUser}/reservations/unfinished`;
+
+    fetch(url, {
+      headers: new Headers({
+        Authorization: process.env.NEXT_PUBLIC_LE_AUTH,
+        "Content-Type": "application/json",
+      }),
+    })
+      .then((response) => {
+        if (response.status === 200) {
+          return response.json();
+        } else {
+          throw new Error("Dados Incorretos");
+        }
+      })
+      .then((result) => setReservationStock(result))
+      .catch((error) => setError(true));
+  };
+
+  return (
+    <div>
+      <h3 className={styles.title}>Minhas Reservas</h3>
+      <h2 className={styles.grid}>
+        {" "}
+        <br />
+        <label type="text">
+          Client:
+          <input
+            className={styles.card}
+            required={true}
+            type="text"
+            value={reservationUser}
+            onChange={(event) => setReservationUser(event.target.value)}
+          ></input>
+        </label>
+        {/* <label type="text">
+          Basic:
+          <input
+            className={styles.card}
+            required={true}
+            type="text"
+            value={reservationBasic}
+            onChange={(event) => setReservationBasic(event.target.value)}
+          ></input>
+        </label> */}
+        <button className={styles.card} onClick={apiCall}>
+          Verificar
+        </button>
+      </h2>
+
+      {/* If ternário abaixo, dentro do HTML única forma de fazer */}
+
+      {isError === true ? (
+        <ErrorPage message={`Verifique as Credenciais`}></ErrorPage>
+      ) : (
+        <div className={styles.grid}>
+          {reservationStock.map((reserve) => (
+            <div className={styles.card} key={reserve.id}>
+              <span>Cliente: {reserve.clientId}</span> <br />
+              <span>Canal: {reserve.channelId}</span> <br />
+              <span>Location: {reserve.locationId}</span> <br />
+              <span>Sku: {reserve.skuId}</span> <br />
+              <span>Pedido: {reserve.orderId}</span> <br />
+              <span>Quantidade: {reserve.quantity}</span> <br />
+              <span>
+                Data:{" "}
+                {format(new Date(reserve.createdAt), "dd/MM/yyyy HH:mm:ss")}
+              </span>
+              <br />
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
