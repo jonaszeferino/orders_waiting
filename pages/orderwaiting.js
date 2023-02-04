c538689567b6ae076658e0a469355bd97f35547e
jonaszeferino <75401113+jonaszeferino@users.noreply.github.com>
2023-02-04 00:33:49 -0300

    waiting page and reservation page almost there

--- pages/orderwaiting.js
+++ pages/orderwaiting.js
@@ -61,7 +61,7 @@ export default function orders() {
           setTotalResults(result.total)
         )
       )
-      .catch((error) => setError(true));
+      .catch((error) => setError(true), setIsLoading(false));
   };
 
   return (
